6c6be00a51c68167c496e8c39d3c7691900a8998
Jacob Delega <jacobdelega@gmail.com>
2025-07-17 21:47:31 -0400

    Ignore playground

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com","images.unsplash.com"]
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'firebasestorage.googleapis.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+            },
+        ],
     }
 };
 
